9b8ff041a0afe8d7dd4350b57a54d9f8da0265ce
sr-hub <spencer.r1@outlook.com>
2020-01-29 16:21:30 -0500

    added a url to the favorites schema

--- app/models/favoriteJoke.js
+++ app/models/favoriteJoke.js
@@ -14,6 +14,10 @@ const favoriteJokeSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
+  },
+  url: {
+    type: String,
+    required: false
   }
 }, {
   timestamps: true
